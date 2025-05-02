3c8a26e783054b9c7eaed075eb8fed1285cbc609
SHravankumar Muchchandi <shravankumarmuchchandi@gmail.com>
2025-05-02 11:09:22 +0530

    Updated on 02-05-2025 11:09AM

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // ✅ correct for Render deployment
+  base: './', // ✅ correct for Render deployment
   plugins: [react()],
   optimizeDeps: {
     include: ["react-helmet-async"],
